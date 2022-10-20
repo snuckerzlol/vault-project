5b37ad50d007db97a5ef8f419e262ee8ab042272
snuckerzlol <williamwahyudiwilliam@gmail.com>
2022-10-21 00:02:32 +0800

    Init contract

--- contracts/multisigwallet.sol
+++ contracts/multisigwallet.sol
@@ -1,1 +1,22 @@
 pragma solidity ^0.4.15;
+
+contract MultiSigWallet {
+    address[] public owners;
+    uint public minVotes;
+    uint public expiryDuration;
+    mapping (uint => Transaction) public pendingTransactions;
+    mapping (uint => Transaction) public confirmedTransactions;
+
+    struct Transaction {
+        mapping (address => uint) voteType;
+        uint expiryTime;
+        address destination;
+        address transaction;
+        string name;
+    }
+
+    constructor (address[] _owners, uint _minVotes) public {
+        owners = _owners;
+        minVotes = _minVotes;
+    }
+}
