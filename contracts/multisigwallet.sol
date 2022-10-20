1d316136faff46e04f01320c918f10f13236130f
Tariq Soliman <tariqsoliman2000@gmail.com>
2022-10-20 14:52:50 +0800

    Added some state variables

--- contracts/multisigwallet.sol
+++ contracts/multisigwallet.sol
@@ -1,1 +1,25 @@
 pragma solidity ^0.4.15;
+
+contract Vault {
+    // An instance of a multi-signature wallet
+
+    struct Transaction {
+        string name;
+        mapping(address => bool) hasVoted;
+        mapping(address => bool) vote;
+        uint256 expiryTime;
+        address payable to;
+    }
+
+    // State variables
+    mapping(address => bool) public isOwner; // Stores owners of wallet
+    uint256 public minVotes; // Stores the minimm votes to approve a tx
+    mapping(uint256 => Transaction) public pendingTransactions;
+    mapping(uint256 => Transaction) public processedTransactions;
+
+    // Errors
+
+    // Events
+
+    // Functions
+}
