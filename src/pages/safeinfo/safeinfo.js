49d61c5d46ccad9fcbdfe34a536e43ddd1e3a53b
Tariq Soliman <tariqsoliman2000@gmail.com>
2022-10-30 16:20:29 +0800

    Can now approve/deny a transaction

--- src/pages/safeinfo/safeinfo.js
+++ src/pages/safeinfo/safeinfo.js
@@ -1,30 +1,35 @@
 import Table from 'react-bootstrap/Table';
-import Button from 'react-bootstrap/Button'
-import './safeinfo.css'
+import Button from 'react-bootstrap/Button';
+import './safeinfo.css';
 import { Form, FloatingLabel } from 'react-bootstrap';
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 
 function TxRow(props) {
+    async function voteTx(approve) {
+        await props.contract.methods.voteTransaction(approve).send();
+    }
 
     return (
-
         <tr>
-          <td>{props.TxNumber}</td>
-          <td>{props.Recepient}</td>
-          <td>{props.TxAmount}</td>
-          <td>{props.Votes}/5</td>
-          <td>
-            <Button className='approve-deny'>Approve</Button>
-            <Button className='approve-deny'>Deny</Button>
-          </td>
+            <td>{props.TxNumber}</td>
+            <td>{props.Recepient}</td>
+            <td>{props.TxAmount}</td>
+            <td>{props.Votes}/5</td>
+            <td>
+                <Button className='approve-deny' onClick={voteTx(true)}>
+                    Approve
+                </Button>
+                <Button className='approve-deny' onClick={voteTx(false)}>
+                    Deny
+                </Button>
+            </td>
         </tr>
-    )
+    );
 }
 // Table content here.
 const TxTableContent = [
-
-    {TxNumber: '1', Recepient:'23243', TxAmount:'0.31', Votes:'2' }
-]
+    { TxNumber: '1', Recepient: '23243', TxAmount: '0.31', Votes: '2' },
+];
 
 // function AddNewRow(props){
 
@@ -32,21 +37,16 @@ const TxTableContent = [
 // }
 
 function Balance(props) {
-
     return (
-
         <div className='balance'>
             <h5>Balance: {props.balance}</h5>
         </div>
-    )
+    );
 }
 
-function PendingTxTable() {
-
+function PendingTxTable(props) {
     return (
-
-        <div> 
-
+        <div>
             <h1 className='fs-3 fw-normal'>Pending Transactions</h1>
             <Table striped bordered hover>
                 <thead>
@@ -63,48 +63,48 @@ function PendingTxTable() {
                     {/* <TxRow TxNumber= '1' Recepient='23243' TxAmount='0.31' Votes='2' />
                     <TxRow TxNumber= '2' Recepient='35278' TxAmount='0.79' Votes='0'/>
                     <TxRow TxNumber= '3' Recepient='99001' TxAmount='0.0004' Votes='1'/> */}
-                    {TxTableContent.map((row) => <TxRow TxNumber= {row.TxNumber} Recepient={row.Recepient} TxAmount={row.TxAmount} Votes={row.Votes}/>)}
-                    
+                    {TxTableContent.map((row) => (
+                        <TxRow
+                            TxNumber={row.TxNumber}
+                            Recepient={row.Recepient}
+                            TxAmount={row.TxAmount}
+                            Votes={row.Votes}
+                            contract={props.contract}
+                        />
+                    ))}
                 </tbody>
-
             </Table>
-
         </div>
-    )
-}   
+    );
+}
 
 function AddNewTx() {
-
     return (
-
         <div className='add-new-tx mt-5'>
-
             <h1 className='fs-3 fw-normal'>Add new transaction</h1>
             <div>
-
-                <FloatingLabel label="Address" className="mb-3">
-                    <Form.Control placeholder="Address" />
+                <FloatingLabel label='Address' className='mb-3'>
+                    <Form.Control placeholder='Address' />
                 </FloatingLabel>
 
-                <FloatingLabel label="Amount" className="mb-3">
-                    <Form.Control placeholder="Amount" />
+                <FloatingLabel label='Amount' className='mb-3'>
+                    <Form.Control placeholder='Amount' />
                 </FloatingLabel>
 
-                <FloatingLabel label="Duration" className="mb-3">
-                    <Form.Control placeholder="Duration" />
+                <FloatingLabel label='Duration' className='mb-3'>
+                    <Form.Control placeholder='Duration' />
                 </FloatingLabel>
 
                 <Button>Add Transaction</Button>
-
             </div>
         </div>
-    )
+    );
 }
 
 export default function SafeInfo(props) {
-    const[safeName, setSafeName] = useState('');
-    const[balance, setBalance] = useState(0);
-    const[transactionCount, setTransactionCount] = useState(0);
+    const [safeName, setSafeName] = useState('');
+    const [balance, setBalance] = useState(0);
+    const [transactionCount, setTransactionCount] = useState(0);
 
     async function getSafeName() {
         const safeName = await props.walletContract.methods.safeName().call();
@@ -117,13 +117,16 @@ export default function SafeInfo(props) {
     }
 
     async function getTransactionCount() {
-        const transactionCount = await props.walletContract.methods.transactionCount().call();
+        const transactionCount = await props.walletContract.methods
+            .transactionCount()
+            .call();
         setTransactionCount(transactionCount);
     }
 
-
     async function getTransactions() {
-        const transactions = await props.walletContract.methods.transactions(0).call();
+        const transactions = await props.walletContract.methods
+            .transactions(0)
+            .call();
         console.log(transactions);
     }
 
@@ -135,13 +138,13 @@ export default function SafeInfo(props) {
     }, []);
 
     return (
-        <div> 
+        <div>
             <div className='safe-info-content mt-3'>
-                <span class="safe-name">{safeName}</span>
-                <Balance balance={balance}/>
-                <PendingTxTable/>
-                <AddNewTx/>
+                <span class='safe-name'>{safeName}</span>
+                <Balance balance={balance} />
+                <PendingTxTable contract={props.walletContract} />
+                <AddNewTx /> // Add props here
             </div>
         </div>
-    )
+    );
 }
